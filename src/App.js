018103098a3a31eae64933e733cccc451a3f1b08
YI YANG <oneyoungyanni@gmail.com>
2022-04-13 17:14:48 +1000

    added fetch data function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
+import logo from "./assets/qantas-logo.png";
+import { getHotels } from "./api/HotelAPI";
+
 function App() {
+  const [hotels, setHotels] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    const fetchHotels = async () => {
+      try {
+        const {
+          data: { results },
+        } = await getHotels();
+        setHotels(results);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchHotels();
+  }, []);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="py-6">
+          <a href="https://www.qantas.com/">
+            <img className="h-8 w-auto sm:h-10" src={logo} alt="qantas logo" />
+          </a>
+        </div>
+      </div>
+      {error ? (
+        <p>Sorry, something went wrong</p>
+      ) : (
+        <>list</>
+      )}
+    </div>
   );
 }
 
