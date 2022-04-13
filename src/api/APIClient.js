091e060d4b816e038d2d9849ee7e052822ec24bc
YI YANG <oneyoungyanni@gmail.com>
2022-04-13 20:48:29 +1000

    added api prod endpoint

--- src/api/APIClient.js
+++ src/api/APIClient.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-export const baseUrl = 'http://localhost:3000/api';
-
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api"
+    : " https://6256a9b3e07d2c9a670a7e5c.mockapi.io/api";
 export const apiClient = axios.create({
-    baseURL: baseUrl
+  baseURL: baseUrl,
 });
